25d7565b9dde0f922217a12bb54522f8d51efea3
melissaraujo00 <140029893+melissaraujo00@users.noreply.github.com>
2023-08-21 16:25:55 -0600

    Add files via upload

---
+++ Clase 3/Ejercicios/Ejercicio2.js
@@ -0,0 +1,9 @@
+let numero = Number(prompt("Ingrese un numero "));
+
+if (numero> 0) {
+    alert("El numero es mayor a 0")
+} else if(numero == 0){
+    alert("El numero es 0")
+}else {
+    alert("Numero es negativo")
+}
