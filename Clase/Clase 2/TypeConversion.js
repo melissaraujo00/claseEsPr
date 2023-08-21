3bf70fc972757da9d6d726ac57610cf94e9e18c0
melissaraujo00 <140029893+melissaraujo00@users.noreply.github.com>
2023-08-21 15:32:44 -0600

    Add files via upload

---
+++ Clase/Clase 2/TypeConversion.js
@@ -0,0 +1,13 @@
+let price = 100;
+
+//COMO CONVERTIR STRING A NUMERO
+// let newValue = parseInt(price)
+// let newValue = +price
+// let newValue = Number(price)
+
+
+//COMO CONVERTIR UN NUMERO A STRING
+// let newValue = price.toString();
+let newValue = String(price);
+
+console.log(newValue, typeof newValue);
